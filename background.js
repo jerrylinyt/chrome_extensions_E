97134bae6b51b79f8925be9d7abe313a189a9116
jerry.lin <jerry.lin@gmail.com>
2018-11-05 23:14:41 +0800

    update setting Page

--- background.js
+++ background.js
@@ -6,33 +6,36 @@
 
 const chromep = new ChromePromise();
 
-chromep.storage.sync.get("allData")
-.then(function (storeData) {
-  chrome.extension.getBackgroundPage().console.log(storeData);
-
-
-
-
-  if (!!storeData && !!storeData.allData) {
-    storeData.allData.forEach(function (data, index) {
-      chrome.contextMenus.create({
-        "title": data.name + "%s",
-        "contexts": ["all"],
-        "onclick": function (info, tab) {
-          chrome.tabs.create({
-            url: data.target.replace("%s", info.selectionText)
-          })
-        }
-      });
-    })
-
-  }
-  chrome.contextMenus.create({
-    "title": "設定頁",
-    "contexts": ["all"],
-    "onclick": function (info, tab) {
-      chrome.tabs.create({ url: "options.html" });
-    }
-  });
-
+//Option onChange Listener
+chrome.storage.onChanged.addListener(() => {
+  chromep.contextMenus.removeAll().then(initContextMenus);
 });
+//create ContextMenus
+function initContextMenus() {
+  chromep.storage.sync.get("allData")
+  .then(function (storeData) {
+    if (!!storeData && !!storeData.allData) {
+      storeData.allData.forEach(function (data) {
+        chrome.contextMenus.create({
+          "title": data.name + "%s",
+          "contexts": ["all"],
+          "onclick": (info) => {
+            chrome.tabs.create({
+              url: data.target.replace("%s", info.selectionText)
+            })
+          }
+        });
+      })
+    }
+    chrome.contextMenus.create({
+      "title": "設定頁",
+      "contexts": ["all"],
+      "onclick": () => {
+        chrome.tabs.create({ url: "options.html" });
+      }
+    });
+  });
+}
+//initSetting
+document.addEventListener('DOMContentLoaded', initContextMenus);
+
--- options.js
+++ options.js
@@ -5,8 +5,41 @@
 // import chromep from 'js/chrome-promise.js';
 
 const chromep = new ChromePromise();
-const defaultSetting = [];
+
+const settings = {
+  default: [
+    {checked: true, name: 'IG_ID', target: 'https://www.instagram.com/%s'},
+  ]
+
+};
 let page = $("#settingInput");
+
+function getNewLine(index, item = {name:'', url:''}) {
+  let nameBlock = $("<div class='col-sm-3 '></div>");
+  let targetBlock = $("<div class='col-sm-6 '></div>");
+
+  let line = $("<div class='form-group row'></div>");
+  let functionBtn = $("<div class='col-sm-1 '></div>");
+  let delBtn = $("<a class='btn btn-danger delete-btn'>刪除</a>");
+  delBtn.click(() => {
+    delBtn.parent().parent().remove();
+  });
+
+  // let cb = $("<input class='form-control' type='checkbox' />").attr("checked", item.enabled).attr("name", `enabled_${index}`);
+  let name = $("<input class='form-control' placeholder='name' />").val(
+      item.name).attr("name", `name_${index}`);
+  let target = $("<input class='form-control' placeholder='target' />").val(
+      item.target).attr("name", `target_${index}`);
+  line
+  .append(functionBtn.append(delBtn))
+  .append(nameBlock.append(name))
+  .append(targetBlock.append(target));
+  return line;
+
+}
+
+
+
 
 
 
@@ -19,56 +52,98 @@ function constructOptions() {
     if (!!storeData && !!storeData.allData) {
       allData = storeData.allData;
     } else {
-      allData = defaultSetting
+      allData = settings.default;
     }
 
     page.empty();
-
     let index = 0;
-    let cbBlock = $("<div class='col-sm-1 '></div>");
 
-    let nameBlock = $("<div class='col-sm-3 '></div>");
-    let targetBlock = $("<div class='col-sm-6 '></div>");
     for (let item of allData) {
-      let line = $("<div class='form-group row'></div>");
-      let cb = $("<input class='form-control' type='checkbox' />").attr("checked", item.enabled).attr("name", `enabled_${index}`);
 
-      let name = $("<input class='form-control' placeholder='name' />").val(item.name).attr("name", `name_${index}`);
-      let target = $("<input class='form-control' placeholder='target' />").val(item.target).attr("name", `target_${index}`);
-      line
-      // .append(cbBlock.clone().append(cb))
-      .append(nameBlock.clone().append(name))
-      .append(targetBlock.clone().append(target));
-      page.append(line);
+      page.append(getNewLine(index, item));
       index++;
     }
   })
 }
 
-//儲存按鈕
+//Save Btn
 $("#save").click(function () {
   let allData = [];
-  $("#settingInput :input").each(function(index, dom) {
+  $("#settingInput :input").each(function (index, dom) {
+    /*
+    * collect input value
+    * ex: name = name_1, url_1, name_2, url_2
+    * */
     let name = dom.name.split("_");
     if (name.length > 1) {
-      let data = allData[name[1]];
+      let arrayIndex = name[1];
+      let data = allData[arrayIndex];
       if (!data) {
         data = {};
       }
-      Object.assign(data, {[name[0]]: dom.value});
-      allData[name[1]] = data;
-
+      if (!!dom.value) {
+        Object.assign(data, {[name[0]]: dom.value});
+        allData[arrayIndex] = data;
+      }
     }
   });
 
+  //save option
   chromep.storage.sync
-  .set({allData:allData})
-  .then(function() {
+  .set({allData: allData.filter(obj => Object.keys(obj).length > 0)})
+  .then(() => {
+    constructOptions();
+    return Promise.resolve();
+  })
+  .then(function () {
     let status = document.getElementById('status');
     status.textContent = 'Options saved.';
-    setTimeout(function() {
+    setTimeout(function () {
       status.textContent = '';
     }, 1500);
   })
 });
+//Reset Btn
+$("#reset").click(() => {
+  chromep.storage.sync.clear()
+  .then(() => {
+    constructOptions();
+    return Promise.resolve();
+  })
+  .then(() => {
+    let status = document.getElementById('status');
+    status.textContent = 'Options reset.';
+    //setDefault option
+    setTimeout(function () {
+      status.textContent = '';
+    }, 1500);
+  })
+
+});
+
+$("#add").click(() => {
+  let index = parseInt($("#settingInput :input:last").attr("name").split("_")[1]) + 1;
+  console.log(index); //FIXME DEBUG LOG
+  page.append(getNewLine(index));
+});
+
+
+
+
+
+
+//initPageSetting
 document.addEventListener('DOMContentLoaded', constructOptions);
+//
+// $(() => {
+//   addEvent()
+// });
+//
+// function addEvent () {
+//   $(".delete-btn").each((index, btn) => {
+//     //delete Btn Function
+//     $(btn).click(() => {
+//       $(btn).parent().parent().remove();
+//     })
+//   })
+// }
